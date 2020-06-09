b910cc74548fc402cd54ed55bdb454826071c4de
shagufta7070 <62218830+shagufta7070@users.noreply.github.com>
2020-06-09 16:06:41 +0530

    Subsequent String
    Given a string s and a string t, check if s is subsequence of t.
    
    A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).

---
+++ Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+
+public static void main(String ar[]) {
+Scanner obj=new Scanner(System.in);
+String s=obj.next();
+String t=obj.next();
+Solution st=new Solution();
+System.out.println(st.h(s,t));
+
+}
+        public static boolean h(String s, String t){
+        int j = 0;
+        for(int i = 0; i < t.length(); i++){
+            // If char found move to next char
+            if(s.charAt(j) == t.charAt(i)){
+                ++j;
+            }
+            // Equal means all the characters of str1 are
+            // found in str2 in order
+            if(j == s.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
